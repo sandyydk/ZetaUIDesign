42959de7938a775318d2293b36e2ef49a9b2794c
Sandeep <sandeep@batch.ly>
2017-09-02 14:41:09 +0530

    Final

---
+++ js/main.js
@@ -0,0 +1,97 @@
+jQuery(document).ready(function(){
+	//cache DOM elements
+	var mainContent = $('.cd-main-content'),
+		header = $('.cd-main-header'),
+		sidebar = $('.cd-side-nav'),
+		sidebarTrigger = $('.cd-nav-trigger'),
+		topNavigation = $('.cd-top-nav'),
+		searchForm = $('.cd-search'),
+		accountInfo = $('.account');
+	var userStory = '{"heading":"These photos are why I am trapped in here","content":"Tokyo, holy excrements from small undefined creatures!We\'re there.<br>First place we had on our mind was Nagin Tower"}';
+					
+	var userStoryObj = JSON.parse(userStory);
+	document.getElementById('storytitle').innerHTML = userStoryObj.heading;	
+	document.getElementById('storycontent').innerHTML = userStoryObj.content;
+	//on resize, move search and top nav position according to window width
+	var resizing = false;
+	moveNavigation();
+	$(window).on('resize', function(){
+		if( !resizing ) {
+			(!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
+			resizing = true;
+		}
+	});
+
+	//on window scrolling - fix sidebar nav
+	var scrolling = false;
+	checkScrollbarPosition();
+	$(window).on('scroll', function(){
+		if( !scrolling ) {
+			(!window.requestAnimationFrame) ? setTimeout(checkScrollbarPosition, 300) : window.requestAnimationFrame(checkScrollbarPosition);
+			scrolling = true;
+		}
+	});
+
+	sidebarTrigger.on('click', function(event){
+		event.preventDefault();
+		$([sidebar, sidebarTrigger]).toggleClass('nav-is-visible');
+	});
+
+	sidebar.children('ul').menuAim({
+        activate: function(row) {
+        	$(row).addClass('hover');
+        },
+        deactivate: function(row) {
+        	$(row).removeClass('hover');
+        },
+        exitMenu: function() {
+        	sidebar.find('.hover').removeClass('hover');
+        	return true;
+        },
+    });
+
+	function checkMQ() {
+		//check if mobile or desktop device
+		return window.getComputedStyle(document.querySelector('.cd-main-content'), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "");
+	}
+
+	function moveNavigation(){
+  		var mq = checkMQ();
+        
+        if ( mq == 'mobile' && topNavigation.parents('.cd-side-nav').length == 0 ) {
+        	detachElements();
+			topNavigation.appendTo(sidebar);
+			searchForm.removeClass('is-hidden').prependTo(sidebar);
+		} else if ( ( mq == 'tablet' || mq == 'desktop') &&  topNavigation.parents('.cd-side-nav').length > 0 ) {
+			detachElements();
+			searchForm.insertAfter(header.find('.cd-logo'));
+			topNavigation.appendTo(header.find('.cd-nav'));
+		}
+		checkSelected(mq);
+		resizing = false;
+	}
+
+	function detachElements() {
+		topNavigation.detach();
+		searchForm.detach();
+	}
+
+	function checkSelected(mq) {
+		//on desktop, remove selected class from items selected on mobile/tablet version
+		if( mq == 'desktop' ) $('.has-children.selected').removeClass('selected');
+	}
+
+	function checkScrollbarPosition() {
+		var mq = checkMQ();
+		
+		if( mq != 'mobile' ) {
+			var sidebarHeight = sidebar.outerHeight(),
+				windowHeight = $(window).height(),
+				mainContentHeight = mainContent.outerHeight(),
+				scrollTop = $(window).scrollTop();
+
+			( ( scrollTop + windowHeight > sidebarHeight ) && ( mainContentHeight - sidebarHeight != 0 ) ) ? sidebar.addClass('is-fixed').css('bottom', 0) : sidebar.removeClass('is-fixed').attr('style', '');
+		}
+		scrolling = false;
+	}
+});
